9b4f9eef804f33e5a23cb4cdff6cef3a6e91ab5a
levos-snr <lewisodero27@gmail.com>
2023-03-29 02:49:28 +0300

    spark portfolio

---
+++ assets/js/education.js
@@ -0,0 +1,105 @@
+AOS.init();
+
+// MOOCs Cards
+
+const moocs = document.querySelector(".moocs");
+const moocscards = [
+ 
+  {
+    title: "Introduction to HTML 5",
+    cardImage: "assets/images/education-page/udacity.svg",
+    moocLink: "https://www.udacity.com/course/intro-to-html-and-css--ud001",
+  },
+  {
+    title: "Introduction to CSS 3",
+    cardImage: "assets/images/education-page/udacity.svg",
+    moocLink: "https://www.udacity.com/course/intro-to-html-and-css--ud001",
+  },
+  {
+    title: "Javascript",
+    cardImage: "assets/images/education-page/udacity.svg",
+    moocLink: "https://www.udacity.com/course/intro-to-javascript--ud803",
+  },
+  {
+    title: "Bootstrap 4",
+    cardImage: "assets/images/education-page/udemy.svg",
+    moocLink: "https://www.udemy.com/course/bootstrap-4-tutorials/",
+  },
+];
+
+
+const showCards = () => {
+  let output = "";
+  moocscards.forEach(
+    ({ title, cardImage, moocLink }) =>
+      (output += `        
+        <div class="col-6 col-md-3 col-sm-4 column" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600" >  
+            <div class="card mb-3 mx-auto">
+               <div class="content">
+                  <div class="content-overlay"></div>
+                    <img src=${cardImage} class="card-img-top content-image">     
+                  <div class="content-details fadeIn-bottom">
+                    <a href="${moocLink}" target="_blank"><i class="fa fa-info-circle fa-2x" aria-hidden="true" style="color: white;"></i></a>                                   
+                  </div>
+                </div>
+                <div class="card-body">
+                    <h6 class="mt-0 py-2 text-center font-weight-bold mooc-title" style="font-size:12px;">${title}</h6>
+                </div>
+            </div>
+        </div>        
+      `)
+  );
+  moocs.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards);
+
+
+
+/* Timeline Section*/
+
+$(function () {
+  window.sr = ScrollReveal();
+
+  if ($(window).width() < 768) {
+    if ($(".timeline-content").hasClass("js--fadeInLeft")) {
+      $(".timeline-content")
+        .removeClass("js--fadeInLeft")
+        .addClass("js--fadeInRight");
+    }
+
+    sr.reveal(".js--fadeInRight", {
+      origin: "right",
+      distance: "300px",
+      easing: "ease-in-out",
+      duration: 800,
+    });
+  } else {
+    sr.reveal(".js--fadeInLeft", {
+      origin: "left",
+      distance: "300px",
+      easing: "ease-in-out",
+      duration: 800,
+    });
+
+    sr.reveal(".js--fadeInRight", {
+      origin: "right",
+      distance: "300px",
+      easing: "ease-in-out",
+      duration: 800,
+    });
+  }
+
+  sr.reveal(".js--fadeInLeft", {
+    origin: "left",
+    distance: "300px",
+    easing: "ease-in-out",
+    duration: 800,
+  });
+
+  sr.reveal(".js--fadeInRight", {
+    origin: "right",
+    distance: "300px",
+    easing: "ease-in-out",
+    duration: 800,
+  });
+});
